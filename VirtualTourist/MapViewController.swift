43a2c6b2a608fa59d88a817523456235a6593ef9
Iceapinan <ice.ap2543@gmail.com>
2017-08-20 20:33:56 +0700

    Create User Interface

---
+++ VirtualTourist/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by IceApinan on 20/8/17.
+//  Copyright © 2017 IceApinan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class MapViewController: UIViewController,MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by IceApinan on 20/8/17.
+//  Copyright © 2017 IceApinan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
